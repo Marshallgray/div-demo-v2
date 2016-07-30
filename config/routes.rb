6375332bb85ab207e4a8a71269eca9bc054e3741
Marshall Gray <marshallgray1992@gmail.com>
2016-07-30 20:28:08 +1000

    added profiles, styling changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
 
   get 'profile3', to: 'pages#profile3'
 
+  get 'profile4', to: 'pages#profile4'
+
+  get 'profile5', to: 'pages#profile5'
+
+  get 'profile6', to: 'pages#profile6'
+
+  get 'profile7', to: 'pages#profile7'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
