f5fe86a70d5a7070d6af8135ab370f21dcf1836a
Marshall Gray <marshallgray1992@gmail.com>
2016-08-04 00:18:16 +1000

    updated assets, modified views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,20 @@ Rails.application.routes.draw do
   resources :strengths
   root 'pages#index'
 
-  get 'pages/profile'
-  get 'profile', to: 'pages#profile'
+  
+  get 'fiona-profile', to: 'pages#fiona-profile'
 
-  get 'profile2', to: 'pages#profile2'
+  get 'kamel-profile2', to: 'pages#kamel-profile2'
 
-  get 'profile3', to: 'pages#profile3'
+  get 'dyllan-profile3', to: 'pages#dyllan-profile3'
 
-  get 'profile4', to: 'pages#profile4'
+  get 'jack-profile4', to: 'pages#jack-profile4'
 
-  get 'profile5', to: 'pages#profile5'
+  get 'kimball-profile5', to: 'pages#kimball-profile5'
 
-  get 'profile6', to: 'pages#profile6'
+  get 'rory-profile6', to: 'pages#rory-profile6'
 
-  get 'profile7', to: 'pages#profile7'
+  get 'shiva-profile7', to: 'pages#shiva-profile7'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
