a1677df6ab44b93fb37aff91089c2a67024ed2de
Marshall Gray <marshallgray1992@gmail.com>
2016-08-16 13:00:47 +1000

    update profile views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,20 @@ Rails.application.routes.draw do
   resources :strengths
   root 'pages#index'
 
-  
+
   get 'fiona-profile', to: 'pages#fiona-profile'
 
-  get 'kamel-profile2', to: 'pages#kamel-profile2'
+  get 'kamel-maouch', to: 'pages#kamel-maouch'
 
-  get 'dyllan-profile3', to: 'pages#dyllan-profile3'
+  get 'dyllan-profile', to: 'pages#dyllan-profile'
 
-  get 'jack-profile4', to: 'pages#jack-profile4'
+  get 'jack-dodd', to: 'pages#jack-dodd'
 
-  get 'kimball-profile5', to: 'pages#kimball-profile5'
+  get 'kimball-gibson', to: 'pages#kimball-gibson'
 
-  get 'rory-profile6', to: 'pages#rory-profile6'
+  get 'rory-ocarrol', to: 'pages#rory-ocarrol'
 
-  get 'shiva-profile7', to: 'pages#shiva-profile7'
+  get 'shiva-manohar', to: 'pages#shiva-manohar'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
